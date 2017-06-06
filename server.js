40873699806e1679277dedebc4cdae721a11a13a
Yoon Lee <yoonlee@Yoons-MacBook-Air.local>
2017-06-06 14:58:44 -0700

    set up handlebars views

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ var app = express();
 
 // View Engine
 app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', exphbs({defaultLayout: 'layout'}));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 // Use morgan
