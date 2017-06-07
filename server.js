881a35b44074934ad27ddc5024a3994f9c3a7096
Yoon Lee <yoonlee@Yoons-MacBook-Air.local>
2017-06-07 00:48:01 -0700

    try port fix

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.use('/', routes);
 app.use('/articles', articles);
 
 // Set Port
-app.set('port', (process.env.PORT || 3000));
+app.set('port', process.env.PORT || 3000);
 
 app.listen(app.get('port'), function () {
   console.log('Server started on port ' + app.get('port'));
